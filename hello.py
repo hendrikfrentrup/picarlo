cba40fad6717cb4ac3eeae68fdc374a031121ec6
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2024-11-19 18:22:08 +1100

    add MIT license

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from picarlo.sim import Config, stringify_the_float
+from picarlo.sim import Config, monte_carlo_pi, stringify_the_float
 
 
 def main() -> None:
@@ -7,6 +7,10 @@ def main() -> None:
     stringify_the_float(config.num_samples)
 
     print(f"starting pi carlo with {config.num_samples} samples!")
+
+    pi = monte_carlo_pi(config.num_samples)
+
+    print(f"pi is approximately {pi}")
 
 
 if __name__ == "__main__":
